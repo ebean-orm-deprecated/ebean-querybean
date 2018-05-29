f0f6ae718f6828af25022a88e5876cb5e17e8c4e
ooknight <ooknight@163.com>
2018-05-29 16:49:15 +0800

    Add findSet method for querybean

--- src/test/java/org/querytest/QCustomerTest.java
+++ src/test/java/org/querytest/QCustomerTest.java
@@ -21,213 +21,253 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class QCustomerTest {
 
-    @Test
-    public void findSingleAttribute() {
-        List<String> names = new QCustomer()
-                .setDistinct(true)
-                .select(QCustomer.alias().name)
-                .status.equalTo(Customer.Status.BAD)
-                .findSingleAttributeList();
-        assertThat(names).isNotNull();
-    }
+  @Test
+  public void findSingleAttribute() {
 
-    @Test
-    public void findIterate() {
-        Customer cust = new Customer();
-        cust.setName("foo");
-        cust.setStatus(Customer.Status.GOOD);
-        cust.save();
-        List<Long> ids = new QCustomer()
-                .status.equalTo(Customer.Status.GOOD)
-                .findIds();
-        assertThat(ids).isNotEmpty();
-        Map<List, Customer> map = new QCustomer()
-                .status.equalTo(Customer.Status.GOOD)
-                .findMap();
-        assertThat(map.size()).isEqualTo(ids.size());
-        QueryIterator<Customer> iterate = new QCustomer()
-                .status.equalTo(Customer.Status.GOOD)
-                .findIterate();
-        try {
-            while (iterate.hasNext()) {
-                Customer customer = iterate.next();
-                customer.getName();
-            }
-        } finally {
-            iterate.close();
-        }
-    }
+    List<String> names = new QCustomer()
+        .setDistinct(true)
+        .select(QCustomer.alias().name)
+        .status.equalTo(Customer.Status.BAD)
+        .findSingleAttributeList();
 
-    @Test
-    public void isEmpty() {
-        new QCustomer()
-                .contacts.isEmpty()
-                .findList();
-        new QCustomer()
-                .contacts.isNotEmpty()
-                .findList();
-    }
+    assertThat(names).isNotNull();
+  }
 
-    @Transactional
-    @Test
-    public void forUpdate() {
-        new QCustomer()
-                .id.eq(42)
-                .forUpdate()
-                .findOne();
-        new QCustomer()
-                .id.eq(42)
-                .forUpdateNoWait()
-                .findOne();
-        new QCustomer()
-                .id.eq(42)
-                .forUpdateSkipLocked()
-                .findOne();
-    }
+  @Test
+  public void findIterate() {
 
-    @Ignore
-    @Test
-    public void arrayContains() {
-        new QContact()
-                .phoneNumbers.contains("4312")
-                .findList();
-        new QCustomer()
-                .contacts.phoneNumbers.contains("4312")
-                .findList();
-    }
+    Customer cust = new Customer();
+    cust.setName("foo");
+    cust.setStatus(Customer.Status.GOOD);
+    cust.save();
 
-    @Test
-    public void setIncludeSoftDeletes() {
-        new QCustomer()
-                .setIdIn(42L)
-                .setIncludeSoftDeletes()
-                .findList();
-    }
+    List<Long> ids = new QCustomer()
+        .status.equalTo(Customer.Status.GOOD)
+        .findIds();
 
-    @Test
-    public void testIdIn() {
-        new QCustomer()
-                .setIdIn("1", "2")
-                .findList();
-        new QCustomer()
-                .id.in(1L, 2L, 3L)
-                .findList();
-    }
+    assertThat(ids).isNotEmpty();
 
-    @Test
-    public void testIn() {
-        new QCustomer()
-                .id.in(34L, 33L)
-                .name.in("asd", "foo", "bar")
-                .registered.in(new Date())
-                .findList();
-    }
 
-    @Test
-    public void testNotIn() {
-        new QCustomer()
-                .id.isIn(34L, 33L)
-                .name.notIn("asd", "foo", "bar")
-                .registered.in(new Date())
-                .findList();
-    }
+    Map<List, Customer> map = new QCustomer()
+        .status.equalTo(Customer.Status.GOOD)
+        .findMap();
 
-    @Test
-    public void testQueryBoolean() {
-        new QCustomer()
-                .name.contains("rob")
-                //.setUseDocStore(true)
-                .setMaxRows(10)
-                .findPagedList();
-        new QCustomer()
-                .inactive.isFalse()
-                .findList();
-    }
+    assertThat(map.size()).isEqualTo(ids.size());
 
-    @Test
-    public void testFindOne() {
-        new QCustomer()
-                .name.isIn("rob", "foo")
-                //.setUseDocStore(true)
-                .setMaxRows(1)
-                .findOne();
-        Optional<Customer> maybe = new QCustomer()
-                .name.contains("rob")
-                //.setUseDocStore(true)
-                .setMaxRows(1)
-                .findOneOrEmpty();
-        maybe.isPresent();
-        new QCustomer()
-                .inactive.isFalse()
-                .findList();
-    }
+    QueryIterator<Customer> iterate = new QCustomer()
+        .status.equalTo(Customer.Status.GOOD)
+        .findIterate();
 
-    @Test
-    public void testDate_lessThan() {
-        assertContains(new QCustomer().registered.lt(new Date()).query(), " where t0.registered < ?");
-        assertContains(new QCustomer().registered.before(new Date()).query(), " where t0.registered < ?");
-        assertContains(new QCustomer().registered.lessThan(new Date()).query(), " where t0.registered < ?");
+    try {
+      while (iterate.hasNext()) {
+        Customer customer = iterate.next();
+        customer.getName();
+      }
+    } finally {
+      iterate.close();
     }
+  }
 
-    @Test
-    public void testDate_lessOrEqualTo() {
-        assertContains(new QCustomer().registered.le(new Date()).query(), " where t0.registered <= ?");
-        assertContains(new QCustomer().registered.lessOrEqualTo(new Date()).query(), " where t0.registered <= ?");
-    }
 
-    @Test
-    public void testDate_greaterThan() {
-        assertContains(new QCustomer().registered.after(new Date()).query(), " where t0.registered > ?");
-        assertContains(new QCustomer().registered.gt(new Date()).query(), " where t0.registered > ?");
-        assertContains(new QCustomer().registered.greaterThan(new Date()).query(), " where t0.registered > ?");
-    }
+  @Test
+  public void isEmpty() {
 
-    @Test
-    public void testDate_greaterOrEqualTo() {
-        assertContains(new QCustomer().registered.ge(new Date()).query(), " where t0.registered >= ?");
-        assertContains(new QCustomer().registered.greaterOrEqualTo(new Date()).query(), " where t0.registered >= ?");
-    }
+    new QCustomer()
+        .contacts.isEmpty()
+        .findList();
 
-    private void assertContains(Query<Customer> query, String match) {
-        query.findList();
-        assertThat(query.getGeneratedSql()).contains(match);
-    }
+    new QCustomer()
+        .contacts.isNotEmpty()
+        .findList();
+  }
 
-    @Test
-    public void testQuery() {
-        QContact contact = QContact.alias();
-        QCustomer cust = QCustomer.alias();
-        new QCustomer()
-                // tune query
-                .select(cust.name)
-                .status.isIn(Customer.Status.BAD, Customer.Status.BAD)
-                .contacts.fetch()
-                // predicates
-                .findList();
-        new QCustomer()
-                // tune query
-                .select(cust.name)
-                .contacts.fetch()
-                // predicates
-                .findList();
-        new QCustomer()
-                // tune query
-                .select(cust.id, cust.name)
-                .contacts.fetch(contact.firstName, contact.lastName, contact.email)
-                // predicates
-                .id.greaterThan(1)
-                .findList();
-        PagedList<Customer> pagedList = new QCustomer()
-                // tune query
-                .select(cust.id, cust.name)
-                .contacts.fetch(contact.firstName, contact.lastName, contact.email)
-                // predicates
-                .id.greaterThan(1)
-                .setFirstRow(20)
-                .setMaxRows(10)
-                .findPagedList();
-        pagedList.getList();
-        pagedList.getList();
+  @Transactional
+  @Test
+  public void forUpdate() {
+
+    new QCustomer()
+        .id.eq(42)
+        .forUpdate()
+        .findOne();
+
+    new QCustomer()
+        .id.eq(42)
+        .forUpdateNoWait()
+        .findOne();
+
+    new QCustomer()
+        .id.eq(42)
+        .forUpdateSkipLocked()
+        .findOne();
+  }
+
+
+  @Ignore
+  @Test
+  public void arrayContains() {
+
+    new QContact()
+        .phoneNumbers.contains("4312")
+        .findList();
+
+    new QCustomer()
+        .contacts.phoneNumbers.contains("4312")
+        .findList();
+  }
+
+  @Test
+  public void setIncludeSoftDeletes() {
+
+    new QCustomer()
+        .setIdIn(42L)
+        .setIncludeSoftDeletes()
+        .findList();
+  }
+
+  @Test
+  public void testIdIn() {
+
+    new QCustomer()
+        .setIdIn("1", "2")
+        .findList();
+
+    new QCustomer()
+        .id.in(1L, 2L, 3L)
+        .findList();
+  }
+
+  @Test
+  public void testIn() {
+    new QCustomer()
+        .id.in(34L, 33L)
+        .name.in("asd", "foo", "bar")
+        .registered.in(new Date())
+        .findList();
+  }
+
+  @Test
+  public void testNotIn() {
+    new QCustomer()
+        .id.isIn(34L, 33L)
+        .name.notIn("asd", "foo", "bar")
+        .registered.in(new Date())
+        .findList();
+  }
+
+  @Test
+  public void testQueryBoolean() {
+
+    new QCustomer()
+        .name.contains("rob")
+        //.setUseDocStore(true)
+        .setMaxRows(10)
+        .findPagedList();
+
+    new QCustomer()
+        .inactive.isFalse()
+        .findList();
+  }
+
+  @Test
+  public void testFindOne() {
+
+    new QCustomer()
+      .name.isIn("rob", "foo")
+      //.setUseDocStore(true)
+      .setMaxRows(1)
+      .findOne();
+
+    Optional<Customer> maybe = new QCustomer()
+      .name.contains("rob")
+      //.setUseDocStore(true)
+      .setMaxRows(1)
+      .findOneOrEmpty();
+
+    maybe.isPresent();
+
+    new QCustomer()
+      .inactive.isFalse()
+      .findList();
+  }
+
+  @Test
+  public void testDate_lessThan() {
+
+    assertContains(new QCustomer().registered.lt(new Date()).query(), " where t0.registered < ?");
+    assertContains(new QCustomer().registered.before(new Date()).query(), " where t0.registered < ?");
+    assertContains(new QCustomer().registered.lessThan(new Date()).query(), " where t0.registered < ?");
+  }
+
+  @Test
+  public void testDate_lessOrEqualTo() {
+
+    assertContains(new QCustomer().registered.le(new Date()).query(), " where t0.registered <= ?");
+    assertContains(new QCustomer().registered.lessOrEqualTo(new Date()).query(), " where t0.registered <= ?");
+  }
+
+  @Test
+  public void testDate_greaterThan() {
+
+    assertContains(new QCustomer().registered.after(new Date()).query(), " where t0.registered > ?");
+    assertContains(new QCustomer().registered.gt(new Date()).query(), " where t0.registered > ?");
+    assertContains(new QCustomer().registered.greaterThan(new Date()).query(), " where t0.registered > ?");
+  }
+
+
+  @Test
+  public void testDate_greaterOrEqualTo() {
+
+    assertContains(new QCustomer().registered.ge(new Date()).query(), " where t0.registered >= ?");
+    assertContains(new QCustomer().registered.greaterOrEqualTo(new Date()).query(), " where t0.registered >= ?");
+  }
+
+  private void assertContains(Query<Customer> query, String match) {
+    query.findList();
+    assertThat(query.getGeneratedSql()).contains(match);
+  }
+
+  @Test
+  public void testQuery() {
+
+    QContact contact = QContact.alias();
+    QCustomer cust = QCustomer.alias();
+
+    new QCustomer()
+        // tune query
+        .select(cust.name)
+        .status.isIn(Customer.Status.BAD, Customer.Status.BAD)
+        .contacts.fetch()
+        // predicates
+        .findList();
+
+    new QCustomer()
+        // tune query
+        .select(cust.name)
+        .contacts.fetch()
+        // predicates
+        .findList();
+
+    new QCustomer()
+        // tune query
+        .select(cust.id, cust.name)
+        .contacts.fetch(contact.firstName, contact.lastName, contact.email)
+        // predicates
+        .id.greaterThan(1)
+        .findList();
+
+    PagedList<Customer> pagedList = new QCustomer()
+        // tune query
+        .select(cust.id, cust.name)
+        .contacts.fetch(contact.firstName, contact.lastName, contact.email)
+        // predicates
+        .id.greaterThan(1)
+        .setFirstRow(20)
+        .setMaxRows(10)
+        .findPagedList();
+
+    pagedList.getList();
+    pagedList.getList();
+
 //    new QCustomer()
 //        .asDraft()
 //        .findList();
@@ -235,6 +275,7 @@ public class QCustomerTest {
 //    new QCustomer()
 //        .includeSoftDeletes()
 //        .findList();
+
 //    List<Contact> contacts
 //        = new QContact()
 //        .email.like("asd")
@@ -243,6 +284,7 @@ public class QCustomerTest {
 //          .id.desc()
 //        .findList();
 //
+
 //    List<Customer> customers = new QCustomer()
 //        .id.eq(1234)
 //        .status.equalTo(Customer.Status.BAD)
@@ -255,6 +297,7 @@ public class QCustomerTest {
 //        .contacts.notes.id.greaterThan(123L)
 //        .orderBy().id.asc()
 //        .findList();
+
 //    //Customer customer3 =
 //    new QCustomer()
 //        .id.gt(12)
@@ -279,14 +322,16 @@ public class QCustomerTest {
 //        .gt("id", 1234)
 //        .disjunction().eq("id", 1234).like("name", "asd")
 //        .endJunction().findList();
+
 //    QCustomer cust = QCustomer.I;
 //    ExpressionList<Customer> expr = new QCustomer().expr();
 //    expr.eq(cust.contacts.email, 123);
-    }
+  }
 
-    @Test
-    public void testFindSet() {
-        Set<Customer> customerSet = new QCustomer().findSet();
-        Assert.assertNotNull(customerSet);
-    }
+  @Test
+  public void testFindSet() {
+
+    Set<Customer> customerSet = new QCustomer().findSet();
+    Assert.assertNotNull(customerSet);
+  }
 }
